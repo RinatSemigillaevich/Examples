c2eeff1ada79506b1fe7838fe7cd1fb0d6b51a71
RinatSemigullaevich <9667070930@mail.ru>
2022-06-26 18:49:08 +0500

    Examples007_домашка

---
+++ Examples007_домашка/Program.cs
@@ -0,0 +1,21 @@
+﻿double Count = 0;
+double Distanse = 10000;
+double FirstFrendSpeed = 1;
+double SecondFrendsSpeed = 2;
+double DogSpeed = 3;
+int Frend = 2;
+
+while(Distanse > 10)
+{
+    if(frend =1);
+    double Time = Distanse / (SecondFrendsSpeed + DogSpeed);
+    else(frend = 2);
+    double Time = Distanse/(FirstFrendSpeed + DogSpeed);
+
+ double Distanse = Distanse - (FirstFrendSpeed + SecondFrendsSpeed) * Time;
+ 
+ int Count ++;
+}
+
+ Console.Write("собака пробежит ");
+ Console.WriteLine("count");
